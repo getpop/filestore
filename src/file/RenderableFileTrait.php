1249fcf111175931aa23c2465b24fef1256d3f15
Leo <losoviz@gmail.com>
2019-05-14 00:23:43 +0800

    Converted getFragmentClasses to getFragmentObjects

--- src/file/RenderableFileTrait.php
+++ src/file/RenderableFileTrait.php
@@ -8,13 +8,11 @@ trait RenderableFileTrait
     public function getFragments()
     {
         if (is_null($this->fragments)) {
-            $this->fragments = array_map(function($class) {
-                return new $class;
-            }, $this->getFragmentClasses());
+            $this->fragments = $this->getFragmentObjects();
         }
         
         return $this->fragments;
     }
 
-    abstract public function getFragmentClasses(): array;
+    abstract protected function getFragmentObjects(): array;
 }
