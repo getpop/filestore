bfaa5b32225d91d802681f765c66b0ce82a47cb7
Leo <losoviz@gmail.com>
2019-05-14 00:41:59 +0800

    Use trait for configuration services

--- src/Component.php
+++ src/Component.php
@@ -1,32 +1,20 @@
 <?php
 namespace PoP\FileStore;
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use PoP\Root\Container\ContainerBuilderFactory;
+use PoP\Root\Component\ConfigurableServicesTrait;
 
 /**
  * Class required to check if this component exists and is active
  */
 class Component
 {
-    /**
-     * Indicate if the component is active
-     *
-     * @var boolean
-     */
-    public static $active;
-    
+    use ConfigurableServicesTrait;
+
     /**
      * Initialize services
      */
     public static function init()
     {
-        self::$active = true;
-
-        // Initialize the ContainerBuilder with this component's service implementations
-        $containerBuilder = ContainerBuilderFactory::getInstance();
-        $loader = new YamlFileLoader($containerBuilder, new FileLocator(dirname(__DIR__)));
-        $loader->load('config/services.yaml');
+        self::initServiceConfiguration(dirname(__DIR__));
     }
 }
