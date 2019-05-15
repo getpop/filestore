51df14900b630dde2044deed2746676f8363ce76
Leo <losoviz@gmail.com>
2019-05-15 23:19:12 +0800

    Renamed trait

--- src/Component.php
+++ src/Component.php
@@ -1,20 +1,20 @@
 <?php
 namespace PoP\FileStore;
 
-use PoP\Root\Component\ConfigurableServicesTrait;
+use PoP\Root\Component\YAMLConfigurableServicesTrait;
 
 /**
  * Class required to check if this component exists and is active
  */
 class Component
 {
-    use ConfigurableServicesTrait;
+    use YAMLConfigurableServicesTrait;
 
     /**
      * Initialize services
      */
     public static function init()
     {
-        self::initServiceConfiguration(dirname(__DIR__));
+        self::initYAMLServiceConfiguration(dirname(__DIR__));
     }
 }
