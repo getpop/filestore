9cc7e0baaf9c7f6e48fb160d419f8dd06544d29a
Leo <losoviz@gmail.com>
2019-05-16 13:09:12 +0800

    Renamed services.php to service-configuration.php

--- src/Component.php
+++ src/Component.php
@@ -1,20 +1,20 @@
 <?php
 namespace PoP\FileStore;
 
-use PoP\Root\Component\YAMLConfigurableServicesTrait;
+use PoP\Root\Component\YAMLServicesTrait;
 
 /**
  * Class required to check if this component exists and is active
  */
 class Component
 {
-    use YAMLConfigurableServicesTrait;
+    use YAMLServicesTrait;
 
     /**
      * Initialize services
      */
     public static function init()
     {
-        self::initYAMLServiceConfiguration(dirname(__DIR__));
+        self::initYAMLServices(dirname(__DIR__));
     }
 }
